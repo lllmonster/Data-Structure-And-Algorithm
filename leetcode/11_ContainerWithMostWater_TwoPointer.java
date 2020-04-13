31ade503e1d4889751a255059e267fd85c815731
Yutong Liu <31671202+lllmonster@users.noreply.github.com>
2020-04-13 12:28:33 -0700

    16

---
+++ leetcode/11_ContainerWithMostWater_TwoPointer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length-1;
+        int area = 0;
+        while (l < r) {
+            area = Math.max(area, Math.min(height[l], height[r])*(r-l));
+            if(height[l] > height[r]) r--;
+            else l++;
+        }
+        return area;
+    }
+}
