1f3bbf8870b43dac75e06094512e3b39b85b058b
Yutong Liu <31671202+lllmonster@users.noreply.github.com>
2022-05-10 17:27:38 -0400

    update lc

--- leetcode/0283_MoveZeros_TwoPointer.java
+++ leetcode/0283_MoveZeros_TwoPointer.java
@@ -14,3 +14,23 @@ class Solution {
         }
     }
 }
+
+/**
+More clear - two pointer o(1) o(n)*/
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int l = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                swap(nums, i, l);
+                l++;
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int l, int r) {
+        int tmp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = tmp;
+    }
+}
